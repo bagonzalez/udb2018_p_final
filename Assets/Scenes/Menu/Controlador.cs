5d46e61438f88747b868a0677b860bd70e8e7058
kevin <sanchez1996kasc@gmail.com>
2018-11-12 23:00:18 -0600

    proyectoFinal-Kevin

---
+++ Assets/Scenes/Menu/Controlador.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void cambiarEscena(string nombre)
+    {
+        print("pasar a la siguiente escena" + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Salir()
+    {
+        print("salir del juego");
+        Application.Quit();
+    }
+
+}
